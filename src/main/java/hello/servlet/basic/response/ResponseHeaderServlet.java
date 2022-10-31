ad1e98a2ed4179e6ee29a8d5818fd4ff80c8af55
DongchannN <ldc991104@gmail.com>
2022-10-31 10:21:24 +0900

    Add ResponseHeaderServlet -Set status -Set response-Header (Content-Type, Cache-Control, my-header...) -How to use Header Method easy -Redirect Concept.

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,61 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //[status-line]
+        //response.setStatus(200);
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //[response-Header]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드]
+        content(response);
+        cookie(response);
+        redirect(response);
+
+        //[Message-body]
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+
+    }
+
+    private void content(HttpServletResponse response) {
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2);  //생략 시 자동 계산
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //second
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+
+        //STATUS 302로 만들고  Location에 주소 삽입 -> redirect.
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
