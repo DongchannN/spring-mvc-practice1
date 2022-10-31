b07ca7d877eae8d8f6fc5244e32362eba7c7b20a
DongchannN <ldc991104@gmail.com>
2022-10-31 14:42:15 +0900

    Add ResponseJsonServlet
    -show the page json type.
    -1.make object(HelloData)
    -2.set username, age(HelloData)
    -3.convert to json.

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ResponseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        //HelloData 객체 생성
+        HelloData data = new HelloData();
+        data.setUsername("Lee");
+        data.setAge(24);
+
+        String result = objectMapper.writeValueAsString(data);
+
+        response.getWriter().write(result);
+    }
+}
