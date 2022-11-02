3d80859bf69e4f9dac71599764279e0762ef219c
DongchannN <ldc991104@gmail.com>
2022-11-02 11:46:51 +0900

    Add MemberRepository -save Member domain by HashMap. -single tone : approach by getInstance() -save(), findById(), findAll(), clearStore()

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,46 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    /**
+     *
+     * instance 라는 MemberRepository 객체 생성.
+     * getInstance() : 매개변수 없이 함수 호출 시 instance 값 반환.
+     * instance 는 static final.
+     *
+     * **/
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+    }
+
+    public Member save(Member member) {
+        member.setId(sequence++);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
